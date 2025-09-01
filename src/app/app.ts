8db774ab43a9536eb31bb554ba7e15347ed7b748
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-01 09:56:15 +0100

    Bind input fields to component properties for note-taking functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,5 +10,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  inputValue = ""
+  texareaInputValue = ""
+  textareaValue = ""
+  isTextareaActive = false
+  activateTextarea(){
+    this.isTextareaActive = true;
+  }
   
 }
