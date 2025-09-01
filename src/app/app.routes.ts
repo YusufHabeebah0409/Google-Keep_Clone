27c8d5a2ab6e1f213828262e1448a1af6396c104
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-01 16:49:42 +0100

    feat: add SignUp and SignIn components with routing and 404 redirect handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { SignUp } from './sign-up/sign-up';
+import { SignIn } from './sign-in/sign-in';
+import { Redirect } from './redirect/redirect';
 
 export const routes: Routes = [
     {path: '', component:Home, title:'Google Keep Clone Dashboard'},
-    {path: '**', redirectTo:''}
+    {path:'Sign-Up', component:SignUp, title:'Google Keep Clone Sign-up'},
+    {path:'Sign-In', component:SignIn, title:'Google Keep Clone Sign-in'},
+    {path: 'Page Not Found', component:Redirect, title:'Page Not Found'},
+    {path: '**', redirectTo:'Page Not Found'}
 ];
